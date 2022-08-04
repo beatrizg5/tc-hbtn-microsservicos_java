29e74e9332d5963412af4cc5c21c3138d82dabaf
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-08-04 14:40:47 -0300

    teste

--- 0x01/spring_boot_2/com/example/Demo2/HelloWorldResource.java
+++ 0x01/spring_boot_2/com/example/Demo2/HelloWorldResource.java
@@ -1,14 +1,17 @@
 package com.example.Demo2;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.PrintStream;
 
 @RestController
 @RequestMapping(value="/messages")
 public class HelloWorldResource {
-    public PrintStream home(){
-        return System.out.printf("PROJETO MAVEN (SPRING INITIALIZR) CRIADO COM SUCESSO !!!");
+
+    @GetMapping("/home")
+    public String home() {
+
+        return "PROJETO MAVEN (SPRING INITIALIZR) CRIADO COM SUCESSO !!!";
+
     }
 }
